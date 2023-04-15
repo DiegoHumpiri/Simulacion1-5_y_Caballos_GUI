49178d45904ce36e1996b6f353fe79f97b960e8f
Diego Johann Humpiri Vargas <dhumpiriv@ulasalle.edu.pe>
2023-04-14 20:36:22 -0500

    Clase caballo para la GUI creada

--- src/threads/caballos/Caballo.java
+++ src/threads/caballos/Caballo.java
@@ -1,5 +1,34 @@
 package threads.caballos;
 
-public class Caballo {
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+
+public class Caballo extends Thread {
+	JProgressBar barraAvance;
+	JTextArea estado;
 	
+	public Caballo(JProgressBar avance, JTextArea estado, String name) {
+		super(name);
+		this.barraAvance = avance;
+		this.estado = estado;
+	}
+	
+	public void run() {
+		this.barraAvance.setValue( 0 );
+		
+		for(int i = 1; i <= 100; i++) {
+			this.barraAvance.setValue(
+				this.barraAvance.getValue() + 1);
+			try {
+				sleep( (int)(Math.random() * 1000) );
+				estado.setText(estado.getText() + 
+					"El caballo " + this.getName() + "descansa.\n");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		estado.setText("Fin de la carrera para:" 
+			+ this.getName() + ".\n");
+	}
 }
